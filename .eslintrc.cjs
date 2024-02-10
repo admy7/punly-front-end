f31b641e1f11846147d5a41eb50d4b617b1d1bca
Younès Adem <63497031+admy7@users.noreply.github.com>
2024-02-10 15:04:42 +0100

    Setup ESLint, Prettier and TailwindCSS (#1)
    feat: setup eslint, prettier and tailwindcss

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,13 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['@typescript-eslint'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
   },
-}
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1> Hello Punly </h1>;
+  return <h1 className="text-3xl text-stone-700">Hello Punly</h1>;
 }
 
 export default App;
